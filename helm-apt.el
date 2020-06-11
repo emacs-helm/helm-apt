654ae817e043f275e6d07b0549c425b756994c95
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2020-06-11 08:55:04 +0200

    Update headers

--- helm-apt.el
+++ helm-apt.el
@@ -1,6 +1,11 @@
 ;;; helm-apt.el --- Helm interface for Debian/Ubuntu packages (apt-*) -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2012 ~ 2018 Thierry Volpiatto <thierry.volpiatto@gmail.com>
+;; Author:      Thierry Volpiatto <thierry.volpiatto@gmail.com>
+
+;; URL: https://github.com/emacs-helm/helm-apt
+;; Package-Requires: ((helm "3.6") (emacs "25.1"))
+;; Version: 1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
